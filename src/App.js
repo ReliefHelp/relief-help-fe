c958e9d8b40304b3aa8f7dc8b1f7091136444b87
NyuydineBill <billleynyuy@gmail.com>
2024-02-02 10:57:49 +0100

    Deployed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 
-import Login from "./components/login pages/Login";
-import Register from "./components/login pages/Register";
 import LandingPage from "./pages/landingpage";
 
 
 function App() {
   return (
     <div className="App font-body">
-      {/* <LandingPage /> */}
-      {/* <Login /> */}
-      <Register />
+      <LandingPage />
     </div>
   );
 }
