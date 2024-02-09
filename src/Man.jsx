afe7c10d19b74fc3b1ad1c881ff3574e9e2aca33
NyuydineBill <billleynyuy@gmail.com>
2024-02-09 10:44:11 +0100

    Added the side bar Not complete yet

--- src/Man.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import "./assets/login styling/man.css";
-
-export default function Man() {
-  return (
-    <div>Man</div>
-  )
-}
--- src/components/landing page/Footer.jsx
+++ src/components/landing page/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { TERipple } from "tw-elements-react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
@@ -9,12 +10,14 @@ export default function Footer() {
           <span className="flex items-center justify-center w-full">
             <span className="mr-4">Sign up for free</span>
             <TERipple rippleColor="light">
+              <Link to="/register">
               <button
                 type="button"
                 className="inline-block rounded-full border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
               >
                 Sign up!
               </button>
+              </Link>
             </TERipple>
           </span>
         </div>
